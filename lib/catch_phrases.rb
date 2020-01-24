5966f23fc92493430d64a9873a83f89e8ae8a2b4
Jacky Lin <jacky.jia.lin@gmail.com>
2020-01-24 01:21:18 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,10 +1,10 @@
 def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
-  return puts phrase = "It's-a me,Mario!"
+  phrase = "It's-a me,Mario!"
+  puts phrase 
 end
 
 def toadstool
-  puts status
+  puts status = 'Thank You Mario! But Our Princess Is In Another Castle!'
 end
 
 def link 
